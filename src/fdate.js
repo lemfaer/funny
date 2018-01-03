b2e15a9483d10c9a1ae52e83a531ada935a772f3
lem <lemfaer@gmail.com>
2018-01-03 12:52:05 +0200

    fix month

--- src/fdate.js
+++ src/fdate.js
@@ -12,7 +12,7 @@ function fdate(timestamp) {
 	var obj = new Date(timestamp * 1000)
 
 	var year = obj.getFullYear()
-	var month = obj.getMonth()
+	var month = obj.getMonth() + 1
 	var day = obj.getDate()
 
 	return sprintf("%04d-%02d-%02d", year, month, day)
