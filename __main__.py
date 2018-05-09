5f96e99f38708ab13cb81490d78d081b35a57532
lem <lemfaer@gmail.com>
2018-05-10 00:26:36 +0300

    -

--- __main__.py
+++ __main__.py
@@ -3,8 +3,6 @@ from args import *
 from db import *
 import json
 
-# /home/lem/Public/funny/python.sh /home/lem/Public/funny/predict --host "127.0.0.1" --base "funny" --user "aroot" --pass "aroot" --id 30
-
 cnx = connect(**base)
 svms, ngrams = select_last_weights(cnx)
 indexes = select_indexes(cnx)
