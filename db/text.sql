444075d5d7d0776b2f6aae3ada3da9b84d6a3b98
lem <lemfaer@gmail.com>
2018-05-12 22:48:37 +0300

    unique hash

--- db/text.sql
+++ db/text.sql
@@ -2,7 +2,7 @@ CREATE TABLE text (
 	id          INTEGER           NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	text        TEXT              CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 	class       VARCHAR(255)      NOT NULL,
-	sha256      VARCHAR(64)       NOT NULL,
+	sha256      VARCHAR(64)       NOT NULL UNIQUE KEY,
 	temp        ENUM("YES", "NO") NOT NULL DEFAULT "YES",
 	created     TIMESTAMP         NOT NULL DEFAULT now(),
 	updated     TIMESTAMP         NOT NULL DEFAULT now() ON UPDATE now()
