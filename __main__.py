caa62b5d365a7e0dbe9afd76a710c75efb81bf80
lem <lemfaer@gmail.com>
2018-05-13 12:43:03 +0300

    fix with no data

--- __main__.py
+++ __main__.py
@@ -1,21 +1,31 @@
 from predict import Predict
+from sys import exit
 from args import *
 from db import *
 import json
 
-cnx = connect(**base)
-svms, ngrams = select_last_weights(cnx)
-indexes = select_indexes(cnx)
-
-predict = Predict(svms, indexes, ngrams)
-uid = predict.text([text]).pop()
-predict.calc()
-
 data = {
-	"type" : predict.type(uid),
-	"ratio" : predict.ratio(uid),
-	"top" : predict.top(),
+	"type" : None,
+	"ratio" : {},
+	"top" : []
 }
 
-data = json.dumps(data)
-print(data)
+try:
+	cnx = connect(**base)
+	svms, ngrams = select_last_weights(cnx)
+	indexes = select_indexes(cnx)
+
+	predict = Predict(svms, indexes, ngrams)
+	uid = predict.text([text]).pop()
+	predict.calc()
+
+	data["ratio"] = predict.type(uid)
+	data["type"] = predict.ratio(uid)
+	data["top"] = predict.top()
+except:
+	pass
+finally:
+	data = json.dumps(data)
+	print(data)
+	exit(0)
+
